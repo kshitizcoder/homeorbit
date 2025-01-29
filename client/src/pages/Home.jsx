f239fdfaaf2f4edf8e55bfeb6ea0d52ef884a260
kshitizcoder <kshitizcareer@gmail.com>
2025-01-29 21:49:21 +0545

    chatapi url remove

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import Hero from "../components/home/Hero";
-import { useGetChatsQuery } from "../redux/chat/chatApi";
+
 import AboutUs from "../components/home/AboutUs";
-import Footer from "../components/home/Footer";
 
 const Home = () => {
   const { data } = useGetChatsQuery();
