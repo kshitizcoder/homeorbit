6904edf0fd8d5254cf5a34b6851cf4d5504200f0
kshitizcoder <kshitizcareer@gmail.com>
2025-01-29 21:45:50 +0545

    remove chatapi

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,21 +3,14 @@ import { apiSlice } from "./Auth/authApi";
 import authSlice from "./Auth/authSlice";
 import { propertyApi } from "./property/propertyApi";
 
-import { chatApi } from "./chat/chatApi";
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authSlice,
     [propertyApi.reducerPath]: propertyApi.reducer,
-
-    [chatApi.reducerPath]: chatApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(
-      apiSlice.middleware,
-      propertyApi.middleware,
-      chatApi.middleware
-    ),
+    getDefaultMiddleware().concat(apiSlice.middleware, propertyApi.middleware),
 });
 
 export default store;
